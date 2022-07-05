529e5bc49e98619f727cea5a2649a453d5907b12
Tomas Morton <tomas.morton@hotmail.co.nz>
2022-07-05 19:42:48 -0400

    Setup base files

---
+++ Project.Euler.FSharp/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Project.Euler.FSharp
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ README.md
@@ -0,0 +1,1 @@
+Template repository containing files always added to new repositories using .NET
